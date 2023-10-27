7ca2ce314585a76a478bc33c21d3a70d7f7b253f
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2023-10-27 17:25:25 -0300

    Add files via upload

--- Tela3.py
+++ Tela3.py
@@ -1,34 +1,37 @@
-
 from Modulos import *
 
-def Contatos():
-    print("Função Contatos chamada.")
 
 TelaReserva = CriarJanela("Reserva", "400x500+650+150", 1)
 TelaReserva.configure(fg_color= "#171717")
-Abas = CriarAbas(TelaReserva, 3, 6, 350, 450, "Reservas", "Revisar pedido", "Contato")
+Abas = CriarAbas(TelaReserva, 3, 6, 350, 450, "Reservas", "Contato")
 Abas.configure(state="normal")
 
-def EnviarDados():
-    Lb_EnviarContatos.configure(text="Dados enviados!")
 
+
+####################### Aba 1 #####################
 def ConfirmarReserva():
     Lb_ConfirmarReserva.configure(text="Reserva confirmada!")
-
-def MudarpRevisar():
-    Abas.set("Revisar pedido")
 
 def MudarpContato():
     Abas.set("Contato")
 
+
+
+####################### Aba 3 #####################
 def VoltarpReservas():
     Abas.set("Reservas")
+
+def EnviarDados():
+    Lb_EnviarContatos.configure(text="Dados enviados!")
+
+
+
+#--------------------------------------------------------------
 
 def Finalizar():
     pass
 
-def VoltarpRevisar():
-    Abas.set("Revisar pedido")
+
 
 
 
@@ -69,11 +72,13 @@ Lb_Mesa2.grid(columnspan=6)
 
 
 #Politicas de cancelamento
-Lb_polCancelamento = CriarLabel(Abas.tab("Reservas"), "Políticas de cancelamento de reservas:", 9, 0)
+Lb_polCancelamento = CriarLabel(Abas.tab("Reservas"), "POLÍTICAS DE CANCELAMENTO:", 9, 0)
+Lb_polCancelamento.configure(text_color="Red")
 Lb_polCancelamento.grid(columnspan=13)
 Lb_polCancelamento2 = CriarLabel(Abas.tab("Reservas"), "Ao desmarcar de última hora, você prejudica"
                                                        "\n a chance de outras pessoas desfrutarem"
                                                        "\n da experiência.", 10, 0)
+Lb_polCancelamento2.configure(text_color="#FFFF00")
 Lb_polCancelamento2.grid(columnspan=13)
 
 
@@ -81,18 +86,7 @@ Lb_polCancelamento2.grid(columnspan=13)
 Lb_ConfirmarReserva = CriarLabel(Abas.tab("Reservas"), "", 11, 6)
 btnConfirmarReserva = CriarBotão(Abas.tab("Reservas"), "Confirmar reserva!", ConfirmarReserva, 12, 3, 30, 30)
 #
-btnMudarpRevisar = CriarBotão(Abas.tab("Reservas"), "Avançar ->", MudarpRevisar, 12, 12, 30, 30)
-
-
-
-
-###########   Revisar pedido   ###########
-
-#Botao
-btnVoltarpRevisar = CriarBotão(Abas.tab("Revisar pedido"), "<- Voltar", VoltarpReservas, 12, 2, 30, 30)
-#
-btnMudarpRevisar = CriarBotão(Abas.tab("Revisar pedido"), "Avançar ->", MudarpContato, 12, 10, 30, 30)
-
+btnMudarpContatos = CriarBotão(Abas.tab("Reservas"), "Avançar ->", MudarpContato, 12, 12, 30, 30)
 
 
 
@@ -106,7 +100,7 @@ Cx_Cad_Nome = CriarCaixaDeTexto(Abas.tab("Contato"), 150, 30, 1, 6, "Insira seu 
 Cx_Cad_Nome.grid(sticky="S")
 
 #Telefone
-Lb_Cad_Tel = CriarLabel(Abas.tab("Contato"), "Telefone:", 2, 5)
+Lb_Cad_Tel = CriarLabel(Abas.tab("Contato"), "Telefone:    ", 2, 5)
 Lb_Cad_Tel.grid(sticky="S")
 Cx_Cad_Tel = CriarCaixaDeTexto(Abas.tab("Contato"), 150, 30, 2, 6, "Insira seu telefone")
 Cx_Cad_Tel.grid(sticky="S")
@@ -118,7 +112,7 @@ Cx_Cad_Email = CriarCaixaDeTexto(Abas.tab("Contato"), 150, 30, 3, 6, "Insira seu
 Cx_Cad_Email.grid(sticky="S")
 
 #Botao
-btnVoltarpRevisar = CriarBotão(Abas.tab("Contato"), "<- Voltar", VoltarpRevisar, 12, 2, 30, 30)
+btnVoltarpReservas = CriarBotão(Abas.tab("Contato"), "<- Voltar", VoltarpReservas, 12, 2, 30, 30)
 #
 Lb_EnviarContatos = CriarLabel(Abas.tab("Contato"), "", 11, 6)
 btnEnviarContatos = CriarBotão(Abas.tab("Contato"), "Enviar", EnviarDados, 12, 10, 30, 30)
